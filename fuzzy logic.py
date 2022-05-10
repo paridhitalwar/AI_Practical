dde9b8b9e215176066a9c3b28d6e511d9ab9c213
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2022-05-10 23:39:30 +0530

    Create fuzzy logic.py

---
+++ fuzzy logic.py
@@ -0,0 +1,8 @@
+fuzzywuzzy import process
+str2Match = "apple inc"
+strOptions = ["Apple Inc.","apple park","apple incorporated","iphone"]
+Ratios = process.extract(str2Match,strOptions)
+print(Ratios)
+# You can also select the string with the highest matching percentage
+highest = process.extractOne(str2Match,strOptions)
+print(highest)
