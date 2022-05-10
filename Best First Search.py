8d9f6e6334b08e108c9a67bb3f7bf06ade900f0b
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2022-05-10 19:19:01 +0530

    Create Best First Search.py

---
+++ Best First Search.py
@@ -0,0 +1,40 @@
+from queue import PriorityQueue
+
+
+# for implementing BFS | returns path having lowest cost
+def best_first_search(source, target, n):
+    visited = [0] * n
+    visited[source] = True
+    pq = PriorityQueue()
+    pq.put((0, source))
+    while pq.empty() == False:
+        u = pq.get()[1]
+        print(u, end=" ") # the path having lowest cost
+        if u == target:
+            break
+
+        for v, c in graph[u]:
+            if visited[v] == False:
+                visited[v] = True
+                pq.put((c, v))
+    print()
+
+# for adding edges to graph
+def addedge(x, y, cost):
+    graph[x].append((y, cost))
+    graph[y].append((x, cost))
+
+
+v = int(input("Enter the number of nodes: "))
+graph = [[] for i in range(v)] # undirected Graph
+e = int(input("Enter the number of edges: "))
+print("Enter the edges along with their weights:")
+for i in range(e):
+    x, y, z = list(map(int, input().split()))
+    addedge(x, y, z)
+    
+
+source = int(input("Enter the Source Node: "))
+target = int(input("Enter the Target/Destination Node: "))
+print("\nPath: ", end = "")
+best_first_search(source, target, v)
