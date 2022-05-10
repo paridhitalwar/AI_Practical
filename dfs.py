1680ef4841f6350dc0d628c73c410e6972540466
Paridhi Talwar <88367084+paridhitalwar@users.noreply.github.com>
2022-05-10 19:15:54 +0530

    Create dfs.py

---
+++ dfs.py
@@ -0,0 +1,24 @@
+# DFS algorithm in Python
+
+import collections
+
+# DFS algorithm
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+
+    print(start)
+
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
+    return visited
+
+
+graph = {'0': set(['1', '2']),
+         '1': set(['0', '3', '4']),
+         '2': set(['0']),
+         '3': set(['1']),
+         '4': set(['2', '3'])}
+print("Following is DFS: ")
+dfs(graph, '0')
